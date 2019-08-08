533fd8ae4ff27bb0fb0a3c377b0fe4749760d1bc
AnLoffredo <38223101+AnLoffredo@users.noreply.github.com>
2019-08-08 10:17:51 -0700

    Add files via upload

---
+++ EmailValidationToken.cs
@@ -0,0 +1,15 @@
+﻿using Penguin.Cms.Entities;
+
+namespace Penguin.Cms.Web.Security
+{
+    /// <summary>
+    /// Used to track whether or not a user has validated their email
+    /// </summary>
+    public class EmailValidationToken : UserAuditableEntity
+    {
+        /// <summary>
+        /// True if the user has validated this token by clicking the link in their email
+        /// </summary>
+        public bool IsValidated { get; set; }
+    }
+}
