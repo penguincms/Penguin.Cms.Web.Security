fa75fb3506742fb05e8c3b5fded3f72fc959fc92
anloffredo <loffredo.ap@gmail.com>
2019-09-30 22:58:43 -0700

    Framework Rework Changes
    Framework Rework Changes

---
+++ Interfaces/ISecurityToken.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Penguin.Cms.Web.Security.Interfaces
+{
+    /// <summary>
+    /// Represents a security validation attempt
+    /// </summary>
+    public interface ISecurityToken
+    {
+        /// <summary>
+        /// If true, access should be allowed
+        /// </summary>
+        bool IsValid { get; }
+    }
+}
